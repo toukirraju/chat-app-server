4338d19f1f026eea8d0ca48a70192a0f2b15113f
unknown <toukirraju@gmail.com>
2023-05-08 22:52:11 +0600

    update server

--- server.js
+++ server.js
@@ -5,28 +5,35 @@ const http = require("http");
 
 const app = express();
 const server = http.createServer(app);
+
 const io = require("socket.io")(server);
 
 global.io = io;
 
 const router = jsonServer.router("db.json");
 
-// response middleware
+//response middleware
 router.render = (req, res) => {
-    const path = req.path;
-    const method = req.method;
-
-    if (
-        path.includes("/conversations") &&
-        (method === "POST" || method === "PATCH")
-    ) {
-        // emit socket event
-        io.emit("conversation", {
-            data: res.locals.data,
-        });
-    }
-
-    res.json(res.locals.data);
+  const path = req.path;
+  const method = req.method;
+  if (
+    path.includes("/conversations") &&
+    (method === "POST" || method === "PATCH")
+  ) {
+    //emit socket event
+    io.emit("conversation", {
+      data: res.locals.data,
+    });
+  } else if (
+    path.includes("/messages") &&
+    (method === "POST" || method === "PATCH")
+  ) {
+    //emit socket event
+    io.emit("message", {
+      data: res.locals.data,
+    });
+  }
+  res.json(res.locals.data);
 };
 
 const middlewares = jsonServer.defaults();
@@ -38,9 +45,9 @@ app.db = router.db;
 app.use(middlewares);
 
 const rules = auth.rewriter({
-    users: 640,
-    conversations: 660,
-    messages: 660,
+  users: 640,
+  conversations: 660,
+  messages: 660,
 });
 
 app.use(rules);
